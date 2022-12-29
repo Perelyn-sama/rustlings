cff42949da9072467909318ed7ef17647b3642ef
Perelyn <pisckid344@gmail.com>
2022-12-29 17:21:16 +0100

    quiz 1 passed

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+pub fn calculate_price_of_apples(amount_of_apples: i32) -> i32 {
+    if amount_of_apples > 40 {
+        let cost: i32 = amount_of_apples;
+        return cost
+    }
+    let cost: i32 = amount_of_apples * 2;
+    cost
+}
 
 // Don't modify this function!
 #[test]
